8ae105b98d895f02dec3c84be7fd3ec2ccb3e9ce
Livin Mathew <livinmathew99@gmail.com>
2015-02-19 11:16:43 +0530

    v1.0

---
+++ IP_Project_Hospital_Management/src/cbse_ip_project_hospital_management/CBSE_IP_Project_Hospital_Management.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cbse_ip_project_hospital_management;
+
+/**
+ *
+ * @author Jithu G Panicker
+ */
+public class CBSE_IP_Project_Hospital_Management {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
